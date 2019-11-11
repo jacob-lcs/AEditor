3e50a90d7fcba272e3915bdaa211c79070b3f8c2
Jacob <40483898+lcs1998@users.noreply.github.com>
2019-11-11 15:56:48 +0800

    :sparkles: 新增重做功能

---
+++ packages/AEditor/src/components/redo/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block" @click="redo">
+    <span class="iconfont" style="font-size:20px">&#xe604;</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Redo',
+  methods: {
+    redo() {
+      document.execCommand('redo')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
