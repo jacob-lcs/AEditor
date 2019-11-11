068aa26d5f00d4a7d6e49c897abf7824ac5e21f8
Jacob <40483898+lcs1998@users.noreply.github.com>
2019-11-11 15:56:36 +0800

    :sparkles: 新增撤销功能

---
+++ packages/AEditor/src/components/undo/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block" @click="undo">
+    <span class="iconfont" style="font-size:25px">&#xe603;</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Undo',
+  methods: {
+    undo() {
+      document.execCommand('undo')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
